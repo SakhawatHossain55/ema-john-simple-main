84af9fa69984101acedb1c6499c6497b9bbbc037
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-18 18:12:43 +0600

    second commit

---
+++ src/components/Review/Review.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import happyImage from '../../images/giphy.gif'
+import { useHistory } from 'react-router';
+
+const Review = () => {
+    const [cart, setCart] = useState([]);
+    const [orderPlaced, setOrderPlaced] = useState(false);
+    const history = useHistory()
+
+    const handelProceedCheckout = () =>{
+        // console.log("place added");
+        history.push('/shipment');
+    }
+
+    const handelRemoveProduct = (productkeys) => {
+        // console.log("clicked product", productkeys);
+        const newCart = cart.filter(pd => pd.key !== productkeys);
+        setCart(newCart);
+        removeFromDatabaseCart(productkeys);
+    }
+
+    useEffect(() => {
+        //cart
+        const savedCart = getDatabaseCart();
+        // console.log(savedCart);//review inside
+        const productkeys = Object.keys(savedCart);
+        // console.log(productkeys);
+
+        const cartProducts = productkeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    }, []);
+    let thankyou;
+    if(orderPlaced){
+        thankyou = <img src={happyImage} alt=""/>
+    }
+    return (
+        <div className="twin-container">
+            {/* <h1>Cart Items: {cart.length}</h1> */}
+            <div className="product-container">
+                {
+                    cart.map(pd => <ReviewItem
+                        key={pd.key}
+                        handelRemoveProduct={handelRemoveProduct}
+                        product={pd}
+                    ></ReviewItem>)
+                }
+                {
+                    thankyou
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handelProceedCheckout} className="main-btn">Proceed Checkout</button>
+                </Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+import { Link } from 'react-router-dom';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productkeys = Object.keys(savedCart);
+        const previousCart = productkeys.map(existingkey => {
+            const product = fakeData.find(pd => pd.key === existingkey);
+            product.quantity = savedCart[existingkey];
+            return product;
+            // console.log(savedCart, savedCart[existingkey]);
+        })
+        // console.log(previousCart);
+        setCart(previousCart);
+    }, [])
+
+    const handelAddProduct = (product) => {
+        // console.log("product addeed", product);
+        const toBeAddedKey = product.key;
+        const sameProduct = cart.find(pd => pd.key === toBeAddedKey)
+        let count = 1;
+        let newCart;
+        if (sameProduct) {
+            const count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== toBeAddedKey);
+            newCart = [...others, sameProduct];
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        setCart(newCart);
+        addToDatabaseCart(product.key, count)
+    }
+
+    return (
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    products.map(pd => <Product
+                        key={pd.key}
+                        showAddToCart={true}
+                        handelAddProduct={handelAddProduct}
+                        product={pd}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <Link to="/review">
+                        <button className="main-btn">Review Order</button>
+                    </Link>
+                </Cart>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
